844abc8aa94f46a9e84d4c1bae44733895764cfb
HectorMG <hector980715@gmail.com>
2023-05-16 19:56:23 -0500

    Se crean middleware para validar rol en las rutas de usaurios

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,5 +1,6 @@
 const { response } = require('express')
 const jwt = require('jsonwebtoken');
+const Usuario = require('../models/usuario.model');
 
 const validarJWT = (req, res = response, next) => {
     const token = req.header('x-token');
@@ -23,7 +24,50 @@ const validarJWT = (req, res = response, next) => {
     }
 }
 
+const validarRol =  async (req, res = response, next) => {
+    const uid = req.uid;
+    try {
+        const usuario = await Usuario.findById(uid)
+    
+        if (usuario.rol !== 'ADMIN_ROLE') {
+            return res.status(403).json({
+                ok:false,
+                msg: "No esta autorizado"
+            });
+        }
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            ok:false,
+            msg: "Token no válido"
+        })
+    }
+}
+
+const validarRolMismoUsuario =  async (req, res = response, next) => {
+    const uid = req.uid;
+    const uidEditar = req.params.id;
+    try {
+        const usuario = await Usuario.findById(uid)
+    
+        if (usuario.rol !== 'ADMIN_ROLE' && uid!==uidEditar ) {
+            return res.status(403).json({
+                ok:false,
+                msg: "No esta autorizado"
+            });
+        }
+        next();
+    } catch (error) {
+        return res.status(401).json({
+            ok:false,
+            msg: "Token no válido"
+        })
+    }
+}
+
 
 module.exports = {
-    validarJWT
+    validarJWT,
+    validarRol,
+    validarRolMismoUsuario
 }
--- app/routes/usuarios.routes.js
+++ app/routes/usuarios.routes.js
@@ -5,11 +5,11 @@ const { Router } = require('express');
 const { getUsuarios, crearUsuarios, actualizarUsuarios, eliminarUsuario } = require('../controllers/usuario.controller')
 const { check } = require('express-validator')
 const { validarCampos } = require('../middleware/validar-campos');
-const { validarJWT } = require('../middleware/auth');
+const { validarJWT, validarRol, validarRolMismoUsuario } = require('../middleware/auth');
 
 const router = Router();
 
-router.get('/', validarJWT ,getUsuarios);
+router.get('/', [validarJWT, validarRol] ,getUsuarios);
 
 router.post('/', [
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
@@ -20,12 +20,13 @@ router.post('/', [
 
 router.put('/:id', [
     validarJWT,
+    validarRolMismoUsuario,
     check('nombre', 'El nombre es obligatorio').not().isEmpty(),
     check('email', 'El email es obligatorio').isEmail(),
     check('rol', 'El rol es obligatorio').not().isEmpty(),
     validarCampos
 ], actualizarUsuarios);
 
-router.delete('/:id',validarJWT,eliminarUsuario);
+router.delete('/:id',[validarJWT,validarRol],eliminarUsuario);
 
 module.exports = router;
